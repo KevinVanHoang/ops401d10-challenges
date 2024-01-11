c73ccea68cad6ac18fa732b6b57f30d9ebf01835
Kevin Hoang <hoang.kevin67@gmail.com>
2024-01-11 11:06:16 -0800

    Update ops3.py

--- ops3.py
+++ ops3.py
@@ -6,46 +6,48 @@
 # Purpose:                      Monitor the status of hosts using ICMP packets
 # Execution:                    ops3.py
 
+# Python
+
+# Script Name:                  Uptime Sensor Tool - Part 2 of 2
+# Author:                       Kevin Hoang
+# Date of latest revision:      1/10/2024
+# Purpose:                      Monitor the status of hosts using ICMP packets
+# Execution:                    uptime_sensor.py
+
 # For time-related functions
 import time  
 
 # For working with date and time
-# Documentation for this can be found in https://docs.python.org/3/library/datetime.html
 from datetime import datetime  
 
 # For working with the operating system (path, file operations)
 import os  
 
 # Third-party library for ICMP ping operations
-# Documentation for this can be found on https://github.com/kyan001/ping3/blob/master/README.md
 from ping3 import ping  
 
 # This is a module for sending emails
 import smtplib
 
-# this allows the creation of email content
+# This allows the creation of email content
 from email.mime.text import MIMEText
 
 # This is the email configuration
-# This is an email that will recive the notification 
-# I would change it to an email you have access to in order to verify.
-recipient_email = 'cruz59349@gmail.com' # testers need to use a different email.
+# This is an email that will receive the notification 
+# Change it to an email you have access to for verification.
+recipient_email = 'myers776@gmail.com'  # testers need to use a different email.
 
-# This checks if ip can be pinged 
-def ip_to_ping(target_ip):
+# This checks if IP can be pinged 
+def check_host_status(target_ip):
     response = ping(target_ip, timeout=1)
     return response is not None
 
-# This logs an event to a txt file
+# This logs an event to a text file
 def log_event(target_ip, target_hostname, timestamp, status):
     # This formats the log entry
-    log_entry = f"{timestamp} - Host {target_ip} ({target_hostname} is { 'up' if status else 'down'})"
+    log_entry = f"{timestamp} - Host {target_ip} ({target_hostname} is {'up' if status else 'down'})"
 
     # This creates a log file in the Documents folder with a timestamped name
-    # The file name could be called for example: ping_log_20240110_153045.txt, the first set of numbers will be the date, the second will be military time (24h)
-
-    # For Linux environment uncomment the log_filename variable below (line 38) and comment the log_filename variable (line 39) 
-    # log_filename = os.path.join(os.path.expanduser("~"), f"ping_log_{datetime.now().strftime('%Y%m%d_%H%M%S')}.txt")
     log_filename = os.path.join(os.path.expanduser("~"), "Documents", "event_log.txt")
     with open(log_filename, 'a') as log_file:
         log_file.write(log_entry + '\n')
@@ -53,48 +55,58 @@ def log_event(target_ip, target_hostname, timestamp, status):
     # Print the log entry
     print(log_entry)
 
-# This sends email notification using a localhost SMTP server
+# This sends an email notification using a localhost SMTP server
 def send_email(subject, body):
-
     msg = MIMEText(body)
-    msg[ 'Subject' ] = subject
-    msg[ 'From' ] = 'johncruzzz1996@gmail.com'
-    msg[ 'To' ] = recipient_email
+    msg['Subject'] = subject
+    msg['From'] = 'stacymyers2000@gmail.com'
+    msg['To'] = recipient_email
 
-    # This is the App password for the gmail email
+    # This is the App password for the Gmail email
     app_password = 'znsr eqhg rtid orxh'
     with smtplib.SMTP('smtp.gmail.com', 587) as server:
         server.starttls()
-        server.login('johncruzzz1996@gmail.com', app_password)
-        server.sendmail('johncruzzz1996@gmail.com', [recipient_email], msg.as_string())
-        
-
+        server.login('stacymyers2000@gmail.com', app_password)
+        server.sendmail('stacymyers2000@gmail.com', [recipient_email], msg.as_string())
 
 def main():
-    # Welcome message for user
+    # Welcome message for the user
     print("Welcome to the Uptime Sensor Tool. This script will evaluate if a host is up or down on the Local Area Network (LAN).")
-    print("The sensor tool will ping continuously, if you want to exit the tool please press ctrl + c.")
+    print("The sensor tool will ping continuously. If you want to exit the tool, please press ctrl + c.")
     
-    # This accepts user input for the target IP address
+    # Accept user input for the target IP address and hostname
     target_ip = input("Please enter an IP address of a host on the LAN: ")
     target_hostname = input("Please enter the hostname associated with the IP address you entered:  ")
    
-    # This loops is used to ping the target every two seconds
+    # This loop is used to ping the target every two seconds
     while True:
-        status = ip_to_ping(target_ip)
+        # Check the status of the host
+        status = check_host_status(target_ip)
 
+        # Check if the previous status exists and has changed
         if hasattr(main, "previous_status") and main.previous_status != status:
+            # Get the current timestamp
             timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+            # Log the event
             log_event(target_ip, target_hostname, timestamp, status)
 
+            # Create an email subject
             subject = f"Host Status Changed: {target_ip} ({target_hostname})"
+            
+            # Create the email body
             body = f"Host status changed at {timestamp}\n\nPrevious Status: {'Up' if main.previous_status else 'Down'}\nCurrent Status: {'Up' if status else 'Down'}"
+            
+            # Send the email notification
             send_email(subject, body)
         
+        # Set the current status as the previous status for the next iteration
         main.previous_status = status
+        
+        # Sleep for 2 seconds before the next iteration
         time.sleep(2)
 
 if __name__ == "__main__":
     # Run the main function when the script is executed
     main()
+
