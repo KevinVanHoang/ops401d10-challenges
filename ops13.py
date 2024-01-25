fe0917447bfdc382dedd043695562409405438ae
KevinVanHoang <hoang.kevin67@gmail.com>
2024-01-24 23:18:52 -0800

    Add ops13.py

---
+++ ops13.py
@@ -0,0 +1,10 @@
+# Python
+
+# Script Name:                  Network Security Tool with Scapy Part 1 of 3
+# Author:                       Kevin Hoang
+# Date of latest revision:      1/22/2024
+# Purpose:                      Script that creates a TCP Port Range Scanner that tests whether a TCP port is open or closed
+# Execution:                    ops11.py
+# Resources:                    https://github.com/codefellows/seattle-cybersecurity-401d10/blob/main/class-06/challenges/DEMO.md
+#                               https://scapy.readthedocs.io/en/latest/introduction.html#
+#                               https://scapy.readthedocs.io/en/latest/extending.html
